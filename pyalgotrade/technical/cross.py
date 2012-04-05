24465eea7f6ae4e26b2ed221cc7d9876f5787324
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2012-04-05 00:54:41 -0300

    Added CrossAbove and CrossBelow technical indicators.

---
+++ pyalgotrade/technical/cross.py
@@ -0,0 +1,104 @@
+# PyAlgoTrade
+# 
+# Copyright 2011 Gabriel Martin Becedillas Ruiz
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. moduleauthor:: Gabriel Martin Becedillas Ruiz <gabriel.becedillas@gmail.com>
+"""
+
+from pyalgotrade import technical
+
+def compute_diff(values1, values2):
+	assert(len(values1) == len(values2))
+	ret = []
+	for i in range(len(values1)):
+		v1 = values1[i]
+		v2 = values2[i]
+		if v1 != None and v2 != None:
+			diff = v1 - v2
+		else:
+			diff = None
+		ret.append(diff)
+	return ret
+
+def possitive(value):
+	return value > 0
+
+def negative(value):
+	return value < 0
+
+class Base(technical.TechnicalIndicatorBase):
+	def __init__(self, ds1, ds2, period, signCheck):
+		assert(period > 1)
+		technical.TechnicalIndicatorBase.__init__(self, 1)
+		self.__ds1 = ds1
+		self.__ds2 = ds2
+		self.__period = period
+		self.__signCheck = signCheck
+
+	def getFirstValidPos(self):
+		return max(self.__ds1.getFirstValidPos(), self.__ds2.getFirstValidPos())
+
+	def getLength(self):
+		return min(self.__ds1.getLength(), self.__ds2.getLength())
+
+	def calculateValue(self, firstPos, lastPos):
+		# Get both set of values.
+		firstPos = max(lastPos - (self.__period - 1), 0)
+		valuesDS1 = self.__ds1.getValuesAbsolute(firstPos, lastPos, True)
+		valuesDS2 = self.__ds2.getValuesAbsolute(firstPos, lastPos, True)
+
+		# Compute differences and check sign changes.
+		ret = 0
+		diffs = compute_diff(valuesDS1, valuesDS2)
+		prevDiff = None
+		for diff in diffs:
+			if prevDiff != None and not self.__signCheck(prevDiff) and self.__signCheck(diff):
+				ret += 1
+			prevDiff = diff
+		return ret
+
+class CrossAbove(Base):
+	"""Checks for a cross above conditions over the specified period between two DataSeries objects.
+
+	It returns the number of times ds1 crossed above ds2 during the given period.
+
+	:param ds1: The DataSeries that crosses.
+	:type ds1: :class:`pyalgotrade.dataseries.DataSeries`.
+	:param ds2: The DataSeries being crossed.
+	:type ds2: :class:`pyalgotrade.dataseries.DataSeries`.
+	:param period: Max number of values to check for cross above conditions. Must be > 1.
+	:type period: int.
+	"""
+
+	def __init__(self, ds1, ds2, period):
+		Base.__init__(self, ds1, ds2, period, possitive)
+
+class CrossBelow(Base):
+	"""Checks for a cross below conditions over the specified period between two DataSeries objects.
+
+	It returns the number of times ds1 crossed below ds2 during the given period.
+
+	:param ds1: The DataSeries that crosses.
+	:type ds1: :class:`pyalgotrade.dataseries.DataSeries`.
+	:param ds2: The DataSeries being crossed.
+	:type ds2: :class:`pyalgotrade.dataseries.DataSeries`.
+	:param period: Max number of values to check for cross below conditions. Must be > 1.
+	:type period: int.
+	"""
+
+	def __init__(self, ds1, ds2, period):
+		Base.__init__(self, ds1, ds2, period, negative)
+
---
+++ testcases/technical_cross_test.py
@@ -0,0 +1,143 @@
+# PyAlgoTrade
+# 
+# Copyright 2011 Gabriel Martin Becedillas Ruiz
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. moduleauthor:: Gabriel Martin Becedillas Ruiz <gabriel.becedillas@gmail.com>
+"""
+
+import unittest
+import copy
+
+from pyalgotrade.technical import cross
+from pyalgotrade import dataseries
+
+class TestCase(unittest.TestCase):
+	def __buildCrossTechnical(self, cls, values1, values2, period):
+		ds1 = dataseries.SequenceDataSeries(values1)
+		ds2 = dataseries.SequenceDataSeries(values2)
+		return cls(ds1, ds2, period)
+
+	def testCrossAboveOnce(self):
+		values1 = [1, 1, 1, 10, 1, 1, 1]
+		values2 = [2, 2, 2,  2, 2, 2, 2]
+
+		# Check every 2 values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 2)
+		for i in range(0, 3):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+		self.assertTrue(crs.getValueAbsolute(3) == 1)
+		for i in range(4, len(values1)):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+
+		# Check every 3 values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 3)
+		for i in range(0, 3):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+		for i in range(3, 5):
+			self.assertTrue(crs.getValueAbsolute(i) == 1)
+		for i in range(5, len(values1)):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+
+		# Check for all values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 100)
+		self.assertTrue(crs.getValue() == 1)
+
+	def testCrossAboveMany(self):
+		count = 100
+		values1 = [-1 if i % 2 == 0 else 1 for i in range(count)]
+		values2 = [0 for i in range(count)]
+
+		# Check every 2 values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 2)
+		self.assertTrue(crs.getValueAbsolute(0) == 0)
+		for i in range(1, count):
+			if i % 2 == 0:
+				self.assertTrue(crs.getValueAbsolute(i) == 0)
+			else:
+				self.assertTrue(crs.getValueAbsolute(i) == 1)
+
+		# Check every 4 values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 4)
+		for i in range(3, count):
+			if i % 2 == 0:
+				self.assertTrue(crs.getValueAbsolute(i) == 1)
+			else:
+				self.assertTrue(crs.getValueAbsolute(i) == 2)
+
+		# Check for all values.
+		crs = self.__buildCrossTechnical(cross.CrossAbove, values1, values2, 100)
+		self.assertTrue(crs.getValue() == count / 2)
+
+	def testCrossBelowOnce(self):
+		values1 = [1, 1, 1, 10, 1, 1, 1]
+		values2 = [2, 2, 2,  2, 2, 2, 2]
+
+		# Check every 2 values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 2)
+		for i in range(0, 4):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+		self.assertTrue(crs.getValueAbsolute(4) == 1)
+		for i in range(5, len(values1)):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+
+		# Check every 3 values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 3)
+		for i in range(0, 4):
+			self.assertTrue(crs.getValueAbsolute(i) == 0)
+		for i in range(4, 6):
+			self.assertTrue(crs.getValueAbsolute(i) == 1)
+
+		self.assertTrue(crs.getValueAbsolute(6) == 0)
+
+		# Check for all values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 100)
+		self.assertTrue(crs.getValue() == 1)
+
+	def testCrossBelowMany(self):
+		count = 100
+		values1 = [-1 if i % 2 == 0 else 1 for i in range(count)]
+		values2 = [0 for i in range(count)]
+
+		# Check every 2 values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 2)
+		self.assertTrue(crs.getValueAbsolute(1) == 0)
+		for i in range(2, count):
+			if i % 2 == 0:
+				self.assertTrue(crs.getValueAbsolute(i) == 1)
+			else:
+				self.assertTrue(crs.getValueAbsolute(i) == 0)
+
+		# Check every 4 values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 4)
+		for i in range(3, count):
+			if i % 2 == 0:
+				self.assertTrue(crs.getValueAbsolute(i) == 2)
+			else:
+				self.assertTrue(crs.getValueAbsolute(i) == 1)
+
+		# Check for all values.
+		crs = self.__buildCrossTechnical(cross.CrossBelow, values1, values2, 100)
+		self.assertTrue(crs.getValue() == count / 2 - 1)
+
+def getTestCases():
+	ret = []
+	ret.append(TestCase("testCrossAboveOnce"))
+	ret.append(TestCase("testCrossAboveMany"))
+	ret.append(TestCase("testCrossBelowOnce"))
+	ret.append(TestCase("testCrossBelowMany"))
+
+	return ret
+
