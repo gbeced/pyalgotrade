810c02f265cbfe0f792941e5471dea2aa31e7969
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2012-04-13 23:03:00 -0300

    Updated version number to 0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PyAlgoTrade',
-	version='0.1',
+	version='0.2',
 	description='Python Algorithmic Trading',
 	author='Gabriel Martin Becedillas Ruiz',
 	author_email='gabriel.becedillas@gmail.com',
