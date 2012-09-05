7b561c71316d88861b8df83e4b1578feed21ce5d
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2012-09-04 21:30:36 -0300

    Added more testcases.

--- testcases/stratanalyzer_test.py
+++ testcases/stratanalyzer_test.py
@@ -32,41 +32,66 @@ import unittest
 import datetime
 import math
 
-class StratAnalyzerTestCase(unittest.TestCase):
+class TradesAnalyzerTestCase(unittest.TestCase):
 	TestInstrument = "spy"
 
 	def __createStrategy(self):
 		barFeed = ninjatraderfeed.Feed(ninjatraderfeed.Frequency.MINUTE)
 		barFilter = csvfeed.USEquitiesRTH()
 		barFeed.setBarFilter(barFilter)
-		barFeed.addBarsFromCSV(StratAnalyzerTestCase.TestInstrument, common.get_data_file_path("nt-spy-minute-2011.csv"))
+		barFeed.addBarsFromCSV(TradesAnalyzerTestCase.TestInstrument, common.get_data_file_path("nt-spy-minute-2011.csv"))
 		return strategy_test.TestStrategy(barFeed, 1000)
 
-	def testBasicAnalyzer(self):
+	def testNoTrades(self):
+		strat = self.__createStrategy()
+		stratAnalyzer = trades.BasicAnalyzer()
+		strat.attachAnalyzer(stratAnalyzer)
+
+		strat.run()
+
+		self.assertTrue(strat.getBroker().getCash() == 1000)
+
+		self.assertTrue(stratAnalyzer.getCount() == 0)
+		self.assertTrue(stratAnalyzer.getEvenCount() == 0)
+		self.assertTrue(stratAnalyzer.getMean() == None)
+		self.assertTrue(stratAnalyzer.getStdDev() == None)
+		self.assertTrue(stratAnalyzer.getStdDev(0) == None)
+
+		self.assertTrue(stratAnalyzer.getWinningCount() == 0)
+		self.assertTrue(stratAnalyzer.getWinningMean() == None)
+		self.assertTrue(stratAnalyzer.getWinningStdDev() == None)
+		self.assertTrue(stratAnalyzer.getWinningStdDev(0) == None)
+
+		self.assertTrue(stratAnalyzer.getLosingCount() == 0)
+		self.assertTrue(stratAnalyzer.getLosingMean() == None)
+		self.assertTrue(stratAnalyzer.getLosingStdDev() == None)
+		self.assertTrue(stratAnalyzer.getLosingStdDev(0) == None)
+
+	def testSomeTrades(self):
 		strat = self.__createStrategy()
 		stratAnalyzer = trades.BasicAnalyzer()
 		strat.attachAnalyzer(stratAnalyzer)
 
 		# Winning trade
-		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 0), strat.enterLong, StratAnalyzerTestCase.TestInstrument, 1) # 127.14
+		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 0), strat.enterLong, TradesAnalyzerTestCase.TestInstrument, 1) # 127.14
 		strat.addPosExit(datetime.datetime(2011, 1, 3, 15, 16), strat.exitPosition) # 127.16
 		# Losing trade
-		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 30), strat.enterLong, StratAnalyzerTestCase.TestInstrument, 1) # 127.2
+		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 30), strat.enterLong, TradesAnalyzerTestCase.TestInstrument, 1) # 127.2
 		strat.addPosExit(datetime.datetime(2011, 1, 3, 15, 31), strat.exitPosition) # 127.16
 		# Winning trade
-		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 38), strat.enterLong, StratAnalyzerTestCase.TestInstrument, 1) # 127.16
+		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 38), strat.enterLong, TradesAnalyzerTestCase.TestInstrument, 1) # 127.16
 		strat.addPosExit(datetime.datetime(2011, 1, 3, 15, 42), strat.exitPosition) # 127.26
 		# Unfinished trade not closed
-		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 47), strat.enterLong, StratAnalyzerTestCase.TestInstrument, 1) # 127.34
+		strat.addPosEntry(datetime.datetime(2011, 1, 3, 15, 47), strat.enterLong, TradesAnalyzerTestCase.TestInstrument, 1) # 127.34
 		strat.run()
 
 		self.assertTrue(round(strat.getBroker().getCash(), 2) == round(1000 + (127.16 - 127.14) + (127.16 - 127.2) + (127.26 - 127.16) - 127.34, 2))
 
 		self.assertTrue(stratAnalyzer.getCount() == 3)
+		self.assertTrue(stratAnalyzer.getEvenCount() == 0)
 		self.assertTrue(round(stratAnalyzer.getMean(), 2) == 0.03)
 		self.assertTrue(round(stratAnalyzer.getStdDev(), 2) == 0.07)
 		self.assertTrue(round(stratAnalyzer.getStdDev(0), 2) == 0.06)
-		self.assertTrue(stratAnalyzer.getEvenCount() == 0)
 
 		self.assertTrue(stratAnalyzer.getWinningCount() == 2)
 		self.assertTrue(round(stratAnalyzer.getWinningMean(), 2) == 0.06)
@@ -78,6 +103,18 @@ class StratAnalyzerTestCase(unittest.TestCase):
 		self.assertTrue(math.isnan(stratAnalyzer.getLosingStdDev()))
 		self.assertTrue(stratAnalyzer.getLosingStdDev(0) == 0)
 
+class SharpeRatioTestCase(unittest.TestCase):
+	def testNoTrades(self):
+		barFeed = yahoofeed.Feed()
+		barFeed.addBarsFromCSV("ige", common.get_data_file_path("sharpe-ratio-test-ige.csv"))
+		strat = strategy_test.TestStrategy(barFeed, 1000)
+		stratAnalyzer = sharpe.SharpeRatio()
+		strat.attachAnalyzer(stratAnalyzer)
+
+		strat.run()
+		self.assertTrue(strat.getBroker().getCash() == 1000)
+		self.assertTrue(stratAnalyzer.getSharpeRatio(0.04, 252) == None)
+	
 	def testSharpeRatioIGE(self):
 		# This testcase is based on an example from Ernie Chan's book:
 		# 'Quantitative Trading: How to Build Your Own Algorithmic Trading Business'
@@ -148,9 +185,12 @@ class StratAnalyzerTestCase(unittest.TestCase):
 
 def getTestCases():
 	ret = []
-	ret.append(StratAnalyzerTestCase("testBasicAnalyzer"))
-	ret.append(StratAnalyzerTestCase("testSharpeRatioIGE"))
-	ret.append(StratAnalyzerTestCase("testSharpeRatioIGE_Broker"))
-	ret.append(StratAnalyzerTestCase("testSharpeRatioIGE_SPY_Broker"))
+	ret.append(TradesAnalyzerTestCase("testNoTrades"))
+	ret.append(TradesAnalyzerTestCase("testSomeTrades"))
+
+	ret.append(SharpeRatioTestCase("testNoTrades"))
+	ret.append(SharpeRatioTestCase("testSharpeRatioIGE"))
+	ret.append(SharpeRatioTestCase("testSharpeRatioIGE_Broker"))
+	ret.append(SharpeRatioTestCase("testSharpeRatioIGE_SPY_Broker"))
 	return ret
 
