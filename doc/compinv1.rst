8976528c0ec607c03a358040c235aed1e078e3a1
Gabriel Becedillas <gabo@Gabriels-MacBook.local>
2012-12-22 23:31:01 -0300

    Added Computational Investing Part I sample for Homework 3.

--- doc/compinv1.rst
+++ doc/compinv1.rst
@@ -27,3 +27,47 @@ The results were: ::
     Std. dev. daily return: 0.0186
     Sharpe ratio: 3.78
 
+Homework 3
+----------
+
+For this assignment I had to build a market simulation tool that loads orders from a file, executes those,
+and prints the results for each day.
+
+The orders file look like this: ::
+
+    2011,1,10,AAPL,Buy,1500,
+    2011,1,13,AAPL,Sell,1500,
+    2011,1,13,IBM,Buy,4000,
+    2011,1,26,GOOG,Buy,1000,
+    2011,2,2,XOM,Sell,4000,
+    2011,2,10,XOM,Buy,4000,
+    2011,3,3,GOOG,Sell,1000,
+    2011,3,3,IBM,Sell,2200,
+    2011,6,3,IBM,Sell,3300,
+    2011,5,3,IBM,Buy,1500,
+    2011,6,10,AAPL,Buy,1200,
+    2011,8,1,GOOG,Buy,55,
+    2011,8,1,GOOG,Sell,55,
+    2011,12,20,AAPL,Sell,1200,
+
+This is the market simulation tool that I built:
+
+.. literalinclude:: ../samples/compinv-3.py
+
+The output looks like this: ::
+
+    First date 2011-01-10 00:00:00
+    Last date 2011-12-20 00:00:00
+    Symbols ['AAPL', 'IBM', 'GOOG', 'XOM']
+    2011-01-10 00:00:00: Portfolio value: $1000000.00
+    2011-01-11 00:00:00: Portfolio value: $998785.00
+    2011-01-12 00:00:00: Portfolio value: $1002940.00
+    2011-01-13 00:00:00: Portfolio value: $1004815.00
+    .
+    .
+    .
+    2011-12-15 00:00:00: Portfolio value: $1113532.00
+    2011-12-16 00:00:00: Portfolio value: $1116016.00
+    2011-12-19 00:00:00: Portfolio value: $1117444.00
+    2011-12-20 00:00:00: Portfolio value: $1133860.00
+
---
+++ samples/compinv-3.py
@@ -0,0 +1,104 @@
+import csv
+import datetime
+import os
+
+from pyalgotrade.barfeed import yahoofeed
+from pyalgotrade.barfeed import csvfeed
+from pyalgotrade import strategy
+from pyalgotrade import broker
+
+class OrdersFile:
+	def __init__(self, ordersFile):
+		self.__orders = {}
+		self.__firstDate = None
+		self.__lastDate = None
+		self.__instruments = []
+
+		# Load orders from the file.
+		reader = csv.DictReader(open(ordersFile, "r"), fieldnames=["year", "month", "day", "symbol", "action", "qty"])
+		for row in reader:
+			dateTime = datetime.datetime(int(row["year"]), int(row["month"]), int(row["day"]))
+			self.__orders.setdefault(dateTime, [])
+			order = (row["symbol"], row["action"], int(row["qty"]))
+			self.__orders[dateTime].append(order)
+
+			# As we process the file, store instruments, first date, and last date.
+			if row["symbol"] not in self.__instruments:
+				self.__instruments.append(row["symbol"])
+
+			if self.__firstDate == None:
+				self.__firstDate = dateTime
+			else:
+				self.__firstDate = min(self.__firstDate, dateTime)
+
+			if self.__lastDate == None:
+				self.__lastDate = dateTime
+			else:
+				self.__lastDate = max(self.__lastDate, dateTime)
+
+	def getFirstDate(self):
+		return self.__firstDate
+
+	def getLastDate(self):
+		return self.__lastDate
+
+	def getInstruments(self):
+		return self.__instruments
+
+	def getOrders(self, dateTime):
+		return self.__orders.get(dateTime, [])
+
+class MyStrategy(strategy.Strategy):
+	def __init__(self, feed, cash, ordersFile):
+		# Suscribe to the feed bars event before the broker just to place the orders properly.
+		feed.getNewBarsEvent().subscribe(self.__onBarsBeforeBroker)
+		# We duplicate the amount of cash to avoid running out of cash.
+		strategy.Strategy.__init__(self, feed, cash * 2)
+		self.__cash = cash
+		self.__ordersFile = ordersFile
+		# We wan't to use using adjuste close instead of close.
+		self.getBroker().setUseAdjustedValues(True)
+
+	def __onBarsBeforeBroker(self, bars):
+		for instrument, action, quantity in self.__ordersFile.getOrders(bars.getDateTime()):
+			if action.lower() == "buy":
+				action = broker.Order.Action.BUY
+			else:
+				action = broker.Order.Action.SELL
+			o = self.getBroker().createMarketOrder(action, instrument, quantity, onClose=True)
+			self.getBroker().placeOrder(o)
+
+	def onOrderUpdated(self, order):
+		execInfo = order.getExecutionInfo()
+		if execInfo:
+			pass
+		else:
+			raise Exception("Order canceled. Ran out of cash ?")
+
+	def onBars(self, bars):
+		# We adjust the portfolio value since we duplicated the amount of cash.
+		portfolioValue = self.getBroker().getValue(bars) - self.__cash
+		print "%s: Portfolio value: $%.2f" % (bars.getDateTime(), portfolioValue)
+
+def main():
+	# Load the orders file.
+	ordersFile = OrdersFile("compinv-3-orders.csv")
+	print "First date", ordersFile.getFirstDate()
+	print "Last date", ordersFile.getLastDate()
+	print "Symbols", ordersFile.getInstruments()
+
+
+	# Load the data from QSTK storage. QS environment variable has to be defined.
+	feed = yahoofeed.Feed()
+	feed.setBarFilter(csvfeed.DateRangeFilter(ordersFile.getFirstDate(), ordersFile.getLastDate()))
+	feed.setDailyBarTime(datetime.time(0, 0, 0)) # This is to match the dates loaded with the ones in the orders file.
+	for symbol in ordersFile.getInstruments():
+		feed.addBarsFromCSV(symbol, os.path.join(os.getenv("QS"), "QSData", "Yahoo", symbol + ".csv"))
+
+	# Run the strategy.
+	cash = 1000000
+	strat = MyStrategy(feed, cash, ordersFile)
+	strat.run()
+
+main()
+
