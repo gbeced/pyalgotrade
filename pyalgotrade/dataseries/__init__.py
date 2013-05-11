f86319ad13d5f184d4c12ca16db3e47dd27c522a
Gabriel Becedillas <gabo@Gabriels-MacBook.local>
2013-05-10 23:51:44 -0300

    Improved aligned dataseries and added additional testcases.

--- pyalgotrade/dataseries/__init__.py
+++ pyalgotrade/dataseries/__init__.py
@@ -188,19 +188,18 @@ def datetime_aligned(ds1, ds2):
 	:param ds2: A DataSeries instance.
 	:type ds2: :class:`DataSeries`
 	"""
-	syncer = Syncer(ds1, ds2)
 	aligned1 = SequenceDataSeries()
 	aligned2 = SequenceDataSeries()
-	syncer.setDestinationDSs(aligned1, aligned2)
+	Syncer(ds1, ds2, aligned1, aligned2)
 	return (aligned1, aligned2)
 
 # This class is responsible for filling 2 dataseries when 2 other dataseries get new values.
 class Syncer:
-	def __init__(self, sourceDS1, sourceDS2):
+	def __init__(self, sourceDS1, sourceDS2, destDS1, destDS2):
 		self.__sourceDS1 = sourceDS1
 		self.__sourceDS2 = sourceDS2
-		self.__destDS1 = None
-		self.__destDS2 = None
+		self.__destDS1 = destDS1
+		self.__destDS2 = destDS2
 		sourceDS1.getNewValueEvent().subscribe(self.__onNewValue1)
 		sourceDS2.getNewValueEvent().subscribe(self.__onNewValue2)
 		# Source dataseries will keep a reference to self and that will prevent from getting this destroyed.
@@ -224,18 +223,16 @@ class Syncer:
 		# If a value for dateTime was added to self.__sourceDS1, and a value for that same datetime is also in self.__sourceDS2
 		# then append to both destination dataseries.
 		if pos2 != None:
-			self.__destDS1.appendWithDateTime(dateTime, value)
-			self.__destDS2.appendWithDateTime(dateTime, self.__sourceDS2[pos2])
+			self.__append(dateTime, value, self.__sourceDS2[pos2])
 
 	def __onNewValue2(self, dataSeries, dateTime, value):
 		pos1 = self.__findPosForDateTime(self.__sourceDS1, dateTime)
 		# If a value for dateTime was added to self.__sourceDS2, and a value for that same datetime is also in self.__sourceDS1
 		# then append to both destination dataseries.
 		if pos1 != None:
-			self.__destDS2.appendWithDateTime(dateTime, value)
-			self.__destDS1.appendWithDateTime(dateTime, self.__sourceDS1[pos1])
+			self.__append(dateTime, self.__sourceDS1[pos1], value)
 
-	def setDestinationDSs(self, ds1, ds2):
-		self.__destDS1 = ds1
-		self.__destDS2 = ds2
+	def __append(self, dateTime, value1, value2):
+		self.__destDS1.appendWithDateTime(dateTime, value1)
+		self.__destDS2.appendWithDateTime(dateTime, value2)
 
--- testcases/dataseries_test.py
+++ testcases/dataseries_test.py
@@ -219,7 +219,6 @@ class TestDateAlignedDataSeries(unittest.TestCase):
 			else:
 				ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
 
-
 		self.assertEqual(ads1.getLength(), ads2.getLength())
 		self.assertEqual(ads1[:], ads2[:])
 		self.assertEqual(ads1.getDateTimes(), commonDateTimes)
@@ -239,6 +238,93 @@ class TestDateAlignedDataSeries(unittest.TestCase):
 			self.assertEqual(ads1[:], ads2[:])
 			self.assertEqual(ads1.getDateTimes()[:], ads2.getDateTimes()[:])
 
+	def testNotAligned_Recursive(self):
+		size = 20
+		ds1 = dataseries.SequenceDataSeries()
+		ds2 = dataseries.SequenceDataSeries()
+		ads1, ads2 = dataseries.datetime_aligned(ds1, ds2)
+		# Align the aligned dataseries again with respect to each other.
+		ads1, ads2 = dataseries.datetime_aligned(ads1, ads2)
+
+		now = datetime.datetime.now()
+		for i in range(size):
+			if i % 2 == 0:
+				ds1.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			else:
+				ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+
+		self.assertEqual(len(ds1), len(ds2))
+
+		for ads in [ads1, ads2]:
+			self.assertEqual(ads.getLength(), 0)
+			self.assertEqual(ads.getFirstValidPos(), 0)
+			self.assertEqual(ads.getValueAbsolute(0), None)
+			self.assertEqual(ads.getDateTimes(), [])
+
+	def testFullyAligned_Recursive(self):
+		size = 20
+		ds1 = dataseries.SequenceDataSeries()
+		ds2 = dataseries.SequenceDataSeries()
+		ads1, ads2 = dataseries.datetime_aligned(ds1, ds2)
+		# Align the aligned dataseries again with respect to each other.
+		ads1, ads2 = dataseries.datetime_aligned(ads1, ads2)
+
+		now = datetime.datetime.now()
+		for i in range(size):
+			ds1.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+
+		self.assertEqual(len(ds1), len(ds2))
+
+		for ads in [ads1, ads2]:
+			self.assertEqual(ads.getLength(), size)
+			self.assertEqual(ads.getFirstValidPos(), 0)
+			for i in range(size):
+				self.assertEqual(ads.getValueAbsolute(i), i)
+				self.assertEqual(ads.getDateTimes()[i], now + datetime.timedelta(seconds=i))
+
+	def testPartiallyAligned_Recursive(self):
+		size = 20
+		commonDateTimes = []
+		ds1 = dataseries.SequenceDataSeries()
+		ds2 = dataseries.SequenceDataSeries()
+		ads1, ads2 = dataseries.datetime_aligned(ds1, ds2)
+		# Align the aligned dataseries again with respect to each other.
+		ads1, ads2 = dataseries.datetime_aligned(ads1, ads2)
+
+		now = datetime.datetime.now()
+		for i in range(size):
+			if i % 3 == 0:
+				commonDateTimes.append(now + datetime.timedelta(seconds=i))
+				ds1.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+				ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			elif i % 2 == 0:
+				ds1.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			else:
+				ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+
+		self.assertEqual(ads1.getLength(), ads2.getLength())
+		self.assertEqual(ads1[:], ads2[:])
+		self.assertEqual(ads1.getDateTimes(), commonDateTimes)
+		self.assertEqual(ads2.getDateTimes(), commonDateTimes)
+
+	def testIncremental_Recursive(self):
+		size = 20
+		ds1 = dataseries.SequenceDataSeries()
+		ds2 = dataseries.SequenceDataSeries()
+		ads1, ads2 = dataseries.datetime_aligned(ds1, ds2)
+		# Align the aligned dataseries again with respect to each other.
+		ads1, ads2 = dataseries.datetime_aligned(ads1, ads2)
+
+		now = datetime.datetime.now()
+		for i in range(size):
+			ds1.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			ds2.appendValueWithDatetime(now + datetime.timedelta(seconds=i), i)
+			self.assertEqual(ads1.getLength(), ads2.getLength())
+			self.assertEqual(ads1[:], ads2[:])
+			self.assertEqual(ads1.getDateTimes()[:], ads2.getDateTimes()[:])
+
+
 def getTestCases():
 	ret = []
 
@@ -257,6 +343,10 @@ def getTestCases():
 	ret.append(TestDateAlignedDataSeries("testFullyAligned"))
 	ret.append(TestDateAlignedDataSeries("testPartiallyAligned"))
 	ret.append(TestDateAlignedDataSeries("testIncremental"))
+	ret.append(TestDateAlignedDataSeries("testNotAligned_Recursive"))
+	ret.append(TestDateAlignedDataSeries("testFullyAligned_Recursive"))
+	ret.append(TestDateAlignedDataSeries("testPartiallyAligned_Recursive"))
+	ret.append(TestDateAlignedDataSeries("testIncremental_Recursive"))
 
 	return ret
 
