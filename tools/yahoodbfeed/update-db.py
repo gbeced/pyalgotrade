b276ffddf09505959341edbbce0e67d32b8dc93d
Gabriel Becedillas <gabo@Gabriels-MacBook.local>
2013-09-24 22:15:30 -0300

    Work in progress on tools to build db feed.

--- tools/yahoodbfeed/update-db.py
+++ tools/yahoodbfeed/update-db.py
@@ -21,46 +21,86 @@
 import sys
 sys.path.append("../..")
 
-from pyalgotrade.barfeed import sqlitefeed
-from pyalgotrade.barfeed import yahoofeed
-from pyalgotrade import marketsession
-from pyalgotrade.tools import yahoofinance
 import pyalgotrade.logger
-
-
-import tempfile
 
 pyalgotrade.logger.file_log = "update-db.log"
 logger = pyalgotrade.logger.getLogger("update-db")
 
-def download_bars(db, symbol, year, timezone):
-	try:
-		# Download bars.
-		csvFile = tempfile.NamedTemporaryFile()
-		csvFile.write(yahoofinance.get_daily_csv(symbol, year))
-		csvFile.flush()
+from pyalgotrade.barfeed import sqlitefeed
+from pyalgotrade.barfeed import yahoofeed
+from pyalgotrade import marketsession
+from pyalgotrade.tools import yahoofinance
 
-		# Load them into a feed.
-		feed = yahoofeed.Feed()
-		feed.addBarsFromCSV(symbol, csvFile.name, timezone)
+import os
 
-		# Put them in the db.
-		db.addBarsFromFeed(feed)
-	except Exception, e:
-		logger.error("Error downloading %s bars for %s: %s" % (symbol, year, str(e)))
+storage = "data"
 
-def update_bars(dbFilePath, symbolsFile, timezone, fromYear, toYear):
+def get_csv_filename(symbol, year):
+	return os.path.join(storage, "%s-%d-yahoofinance.csv" % (symbol, year))
+
+def download_files_for_symbol(symbol, fromYear, toYear):
+	if not os.path.exists(storage):
+		logger.info("Creating %s directory" % (storage))
+		os.mkdir(storage)
+
+	status = ""
+	for year in range(fromYear, toYear+1):
+		fileName = get_csv_filename(symbol, year)
+		if not os.path.exists(fileName):
+			logger.info("Downloading %s %d to %s" % (symbol, year, fileName))
+			try: 
+				yahoofinance.download_daily_bars(symbol, year, fileName)
+				status += "1"
+			except Exception, e:
+				logger.error(str(e))
+				status += "0"
+		else:
+			status += "1"
+
+	if status.find("1") == -1:
+		logger.fatal("No data found for %s" % (symbol))
+	elif status.lstrip("0").find("0") != -1:
+		logger.fatal("Some bars are missing for %s" % (symbol))
+
+def download_files(symbolsFile, fromYear, toYear):
+	for symbol in open(symbolsFile, "r"):
+		symbol = symbol.strip()
+		download_files_for_symbol(symbol, fromYear, toYear)
+
+def update_db_for_symbol(db, symbol, timezone, fromYear, toYear):
+	feed = yahoofeed.Feed(timezone)
+	feed.sanitizeBars(True)
+	for year in range(fromYear, toYear+1):
+		fileName = get_csv_filename(symbol, year)
+		if os.path.exists(fileName):
+			feed.addBarsFromCSV(symbol, fileName, timezone)
+	db.addBarsFromFeed(feed)
+
+def update_db(dbFilePath, symbolsFile, timezone, fromYear, toYear):
 	db = sqlitefeed.Database(dbFilePath)
 	for symbol in open(symbolsFile, "r"):
 		symbol = symbol.strip()
-		logger.info("Downloading %s bars" % (symbol))
-		for year in range(fromYear, toYear+1):
-			download_bars(db, symbol, year, timezone)
+		logger.info("Updating %s bars" % (symbol))
+		update_db_for_symbol(db, symbol, timezone, fromYear, toYear)
+
+# Symbols
+# http://www.nasdaq.com/screening/companies-by-name.aspx?exchange=NYSE&render=download
+# http://www.nasdaq.com/screening/companies-by-name.aspx?exchange=NASDAQ&render=download
 
 def main():
 	fromYear = 2000
 	toYear = 2012
-	update_bars("yahoofinance-sp500.sqlite", "sp500-symbols.txt", marketsession.USEquities.timezone, fromYear, toYear)
+
+	symbolsFile = "nyse-symbols.txt"
+	dbFile = "nyse.sqlite"
+	timezone = marketsession.USEquities.timezone
+
+	# symbolsFile = "merval-symbols.txt"
+	# dbFile = "merval.sqlite"
+	# timezone = marketsession.MERVAL.timezone
+
+	# download_files(symbolsFile, fromYear, toYear)
+	# update_db(dbFile, symbolsFile, timezone, fromYear, toYear)
 
 main()
 
