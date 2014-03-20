7c69165a3605a704a73eb59d42c0140b5d93035d
Gabriel Becedillas <gabo@Gabriels-MacBook.local>
2014-03-19 22:31:06 -0300

    Updated copyright notice.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyAlgoTrade'
-copyright = u'2011-2013, Gabriel Martín Becedillas Ruiz'
+copyright = u'2011-2014, Gabriel Martín Becedillas Ruiz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
