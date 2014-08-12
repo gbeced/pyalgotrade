d55799ced51f13379f2fda1ae068dd334191c07f
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2014-08-11 23:32:08 -0300

    Improving code coverage.

---
+++ testcases/twitter_test.py
@@ -0,0 +1,63 @@
+# PyAlgoTrade
+#
+# Copyright 2011-2014 Gabriel Martin Becedillas Ruiz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. moduleauthor:: Gabriel Martin Becedillas Ruiz <gabriel.becedillas@gmail.com>
+"""
+
+import unittest
+import datetime
+
+from pyalgotrade import dispatcher
+from pyalgotrade.twitter import feed as twitterfeed
+
+import twitter_credentials
+
+
+class TwitterFeedTestCase(unittest.TestCase):
+    def testTwitterFeed(self):
+        events = {
+            "on_tweet": False,
+            "start": datetime.datetime.now()
+        }
+        disp = dispatcher.Dispatcher()
+
+        def on_tweet(data):
+            events["on_tweet"] = True
+            disp.stop()
+
+        def on_idle():
+            # Stop after 5 minutes.
+            if (datetime.datetime.now() - events["start"]).seconds > 60*5:
+                disp.stop()
+
+        # Create a twitter feed to track BitCoin related events.
+        consumer_key = twitter_credentials.consumer_key
+        consumer_secret = twitter_credentials.consumer_secret
+        access_token = twitter_credentials.access_token
+        access_token_secret = twitter_credentials.access_token_secret
+        track = ["bitcoin", "btc"]
+        follow = []
+        languages = ["en"]
+        twitterFeed = twitterfeed.TwitterFeed(consumer_key, consumer_secret, access_token, access_token_secret, track, follow, languages)
+
+        disp.addSubject(twitterFeed)
+        twitterFeed.subscribe(on_tweet)
+        disp.getIdleEvent().subscribe(on_idle)
+        disp.run()
+
+        # Check that we received both events.
+        self.assertTrue(events["on_tweet"])
