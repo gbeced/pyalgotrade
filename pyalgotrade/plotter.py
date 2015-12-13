e60d5c0ef3ffa0f56c5b6cdac61105bec45b4e58
Kimble Young <kimbleyoung@yahoo.com.au>
2015-12-13 14:21:15 +1100

    added save plot function

--- pyalgotrade/plotter.py
+++ pyalgotrade/plotter.py
@@ -443,3 +443,10 @@ class StrategyPlotter(object):
         fig, mplSubplots = self.__buildFigureImpl(fromDateTime, toDateTime)
         fig.autofmt_xdate()
         plt.show()
+
+    def savePlot(self,filename, fromDateTime=None, toDateTime=None):
+
+        fig, mplSubplots = self.__buildFigureImpl(fromDateTime, toDateTime)
+        fig.autofmt_xdate()
+
+        fig.savefig(filename, bb_inches='tight')
