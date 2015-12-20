be526bc4aed66258213bf0a133f5fdac2162c038
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2015-12-20 00:47:03 -0300

    Work in progress on backtesting broker.

--- pyalgotrade/coinbase/broker.py
+++ pyalgotrade/coinbase/broker.py
@@ -18,13 +18,46 @@
 .. moduleauthor:: Gabriel Martin Becedillas Ruiz <gabriel.becedillas@gmail.com>
 """
 
+from pyalgotrade import broker
 from pyalgotrade.broker import backtesting
 from pyalgotrade.coinbase import common
 
 
+class Commission(backtesting.Commission):
+    def calculate(self, order, price, quantity):
+        ret = 0
+        # We're assuming that limit orders never get filled immediately.
+        if order.getType() == broker.Order.Type.MARKET:
+            ret = 0.0025
+        return ret
+
+
 class BacktestingBroker(backtesting.Broker):
+    """
+    A Coinbase backtesting broker.
+
+    :param cash: The initial amount of cash.
+    :type cash: int/float.
+    :param barFeed: The bar feed that will provide the bars.
+    :type barFeed: :class:`pyalgotrade.barfeed.BarFeed`
+
+    .. note::
+        * Only market and limit orders are supported.
+        * Orders are automatically set as **goodTillCanceled=True** and  **allOrNone=False**.
+        * BUY_TO_COVER orders are mapped to BUY orders.
+        * SELL_SHORT orders are mapped to SELL orders.
+    """
+
     def __init__(self, cash, barFeed):
         backtesting.Broker.__init__(self, cash, barFeed)
+        self.setCommission(Commission())
 
     def getInstrumentTraits(self, instrument):
         return common.BTCTraits()
+
+    def createStopOrder(self, action, instrument, stopPrice, quantity):
+        raise Exception("Stop orders are not supported")
+
+    def createStopLimitOrder(self, action, instrument, stopPrice, limitPrice, quantity):
+        raise Exception("Stop limit orders are not supported")
+
