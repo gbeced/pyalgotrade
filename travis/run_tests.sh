3a90d6956fbf0bd1aea27820c42c9304cc932214
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2016-03-11 23:32:18 -0300

    Improving dockerized builds on Travis.

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -3,5 +3,5 @@
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 ls -al
-nosetests --ignore-files=optimizer_testcase --with-coverage --cover-package=pyalgotrade
+nosetests --with-coverage --cover-package=pyalgotrade
 ls -al
