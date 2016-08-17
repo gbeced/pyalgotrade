b66224ce45dfb4f5a61827d1c12cdf8c566f61ad
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2016-08-17 07:42:32 -0300

    Fixed docker image building

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 docker build -t pyalgotrade:0.18 .
 # docker rmi $(docker images --quiet --filter "dangling=true")
 
-# docker tag pyalgotrade:0.18 gbecedillas/pyalgotrade:0.18
+# docker tag -f pyalgotrade:0.18 gbecedillas/pyalgotrade:0.18
 # docker login --username=gbecedillas --email=gbecedillas@gmail.com
 # docker push gbecedillas/pyalgotrade:0.18
 
