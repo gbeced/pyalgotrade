397fc34a8eb8d844ae9eb4b391956a092cbaa1e4
Glenn <glenn+snow_flake@mercury-wireless.com>
2017-10-12 20:33:21 -0400

    Polishing the tests

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -4,4 +4,6 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 # This is needed to avoid "Coverage.py warning: No data was collected" from cov plugin.
 export PYTHONPATH=.
 
-nosetests --with-cov --cov=pyalgotrade --cov-config=coverage.cfg -x
+pip install -U /app
+
+nosetests --with-cov --cov=pyalgotrade --cov-config=coverage.cfg
