873f3a0306cdfd7b22b0e86675ee9b8447961383
osarwar <owais.sarwar@gmail.com>
2021-08-16 13:58:32 -0500

    updated level file to match

--- pyalgotrade/technical/level.py
+++ pyalgotrade/technical/level.py
@@ -1,7 +1,7 @@
 # Use Level class to set a buy or sell level for an indicator, e.g.
 # sell if RSI > 80
 
-class Level:
+class LEVEL:
     
     def __init__(self, level):
         self.level = level
