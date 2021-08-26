337a2c7a63b88bfc6575511417dd8cc0ac298e8d
osarwar <owais.sarwar@gmail.com>
2021-08-26 14:46:14 -0500

    updated stop_price

--- pyalgotrade/technical/stop_price.py
+++ pyalgotrade/technical/stop_price.py
@@ -23,7 +23,7 @@ class SMAEventWindow_Frac(technical.EventWindow):
                 self.__value = self.__value + value / float(self.getWindowSize()) - firstValue / float(self.getWindowSize())
 
     def getValue(self):
-        return self.__value * self.fracOfEntry
+        return self.__value * (self.percent_change + 100) / 100
 
 
 class STOP_PRICE(technical.EventBasedFilter):
@@ -31,16 +31,16 @@ class STOP_PRICE(technical.EventBasedFilter):
 
     :param dataSeries: The DataSeries instance being filtered.
     :type dataSeries: :class:`pyalgotrade.dataseries.DataSeries`.
-    :param fracOfEntry: Fraction of the entry price that should be used to calcualte STOP_PRICE = fracOfEntry * Price.
-    :type fracOfEntry: float.
+    :param percent_change: Percent Change in entry price used to calculate STOP_PRICE = (percent_change + 100 ) / 100 * Price.
+    :type percent_change: float.
     :param maxLen: The maximum number of values to hold.
         Once a bounded length is full, when new items are added, a corresponding number of items are discarded from the
         opposite end. If None then dataseries.DEFAULT_MAX_LEN is used.
     :type maxLen: int.
     """
-    def __init__(self, dataSeries, fracOfEntry, maxLen=None):
-        assert fracOfEntry >= 0, "fracOfEntry must be non-negtive"
-        self.fracOfEntry = fracOfEntry
-        super(STOP_PRICE, self).__init__(dataSeries, SMAEventWindow_Frac(fracOfEntry, period=1), maxLen)
+    def __init__(self, dataSeries, percent_change, maxLen=None):
+        assert percent_change >= -100, "percent_change must be greater than -100"
+        self.percent_change = percent_change
+        super(STOP_PRICE, self).__init__(dataSeries, SMAEventWindow_Frac(percent_change, period=1), maxLen)
 
 
