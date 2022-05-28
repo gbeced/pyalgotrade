3f516315ed02965e5bf89ede583c1e192c2f5807
osarwar <owais.sarwar@gmail.com>
2022-05-28 00:42:54 -0500

    some mad change

--- pyalgotrade/technical/mad.py
+++ pyalgotrade/technical/mad.py
@@ -8,24 +8,25 @@ class MAD(object):
 
     :param dataSeries: The DataSeries instance being filtered for the SMAs in MAD.
     :type dataSeries: :class:`pyalgotrade.dataseries.DataSeries`.
-    :param fast_period: The number of values to use to calculate the MAD.
+    :param fastSMA_period: The number of values to use to calculate the MAD.
     :type period: int.
-    :param slow_period: The number of values to use to calculate the MAD.
+    :param slowSMA_period: The number of values to use to calculate the MAD.
     :type period: int.
     :param maxLen: The maximum number of values to hold.
         Once a bounded length is full, when new items are added, a corresponding number of items are discarded from the
         opposite end. If None then dataseries.DEFAULT_MAX_LEN is used.
     :type maxLen: int.
     """
-    def __init__(self, dataSeries, fast_period=21, slow_period=200, maxLen=None):
+    def __init__(self, dataSeries, fastSMA_period=21, slowSMA_period=200, maxLen=None):
         
-        self.__sma_fast = ma.SMA(dataSeries, fast_period, maxLen=maxLen)
-        self.__sma_slow = ma.SMA(dataSeries, slow_period, maxLen=maxLen)
+        self.__sma_fast = ma.SMA(dataSeries, fastSMA_period, maxLen=maxLen)
+        self.__sma_slow = ma.SMA(dataSeries, slowSMA_period, maxLen=maxLen)
         
         self.__mad = dataseries.SequenceDataSeries(maxLen)
         
         # It is important to subscribe after sma and stddev since we'll use those values.
         dataSeries.getNewValueEvent().subscribe(self.__onNewValue)
+        
         
     def __onNewValue(self, dataseries, dateTime, value):
         
