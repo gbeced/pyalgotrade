f58bbe04b2393ed0eb1c1db48ba8c3ad01117d24
Connor Valentine <cvalentine91@gmail.com>
2022-06-24 10:59:05 -0400

    boolean 2 - electric boogaloo

--- pyalgotrade/technical/boolean2.py
+++ pyalgotrade/technical/boolean2.py
@@ -1,10 +1,10 @@
 # Use Boolean class to set a boolean to compare to an indicator, e.g.
 # sell if PSAR.reversal_toUptrend == True
 
-class BOOLEAN:
+class BOOLEAN2:
     
     def __init__(self, boolean):
-        self.boolean = boolean
+        self.boolean2 = boolean
         
     def getValue(self):
-        return self.boolean
+        return self.boolean2
